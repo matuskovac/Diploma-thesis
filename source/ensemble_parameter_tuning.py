3591512f87d24f3e48c6880f23f869ba9927b177
Matus Kovac <matus.kovac2@gmail.com>
2021-04-03 23:55:19 +0200

    improve ensembling tuning

--- source/ensemble_parameter_tuning.py
+++ source/ensemble_parameter_tuning.py
@@ -33,7 +33,7 @@ for i in range(2, 3):
     comb = list(itertools.combinations(models, i))
     all_comb_models += [list(elem) for elem in comb]
 
-predict_based_on_whole_pattern = True
+all_predict_based_on_whole_pattern = [True, False]
 all_kind_of_patten = [0, 1, 2]
 all_ensemble_based_on_segments = [True, False]
 all_fun = ['max', 'min', 'sum', 'prod']
@@ -41,11 +41,11 @@ all_scale_functions = ['use_standard_scaler_list', 'use_minmax_scaler_list']
 all_count_of_owners = list(range(1, 5))
 
 iterables = [all_count_of_owners, all_comb_models, all_ensemble_based_on_segments,
-             all_fun, all_scale_functions, all_kind_of_patten]
+             all_fun, all_scale_functions, all_kind_of_patten, all_predict_based_on_whole_pattern]
 
 rows = []
 
-for count_of_owners, models_to_use, ensemble_based_on_segments, fun, scale, kind_of_patten in itertools.product(*iterables):
+for count_of_owners, models_to_use, ensemble_based_on_segments, fun, scale, kind_of_patten, predict_based_on_whole_pattern in itertools.product(*iterables):
     users_to_cv = postprocess.get_combinations_for_cv(
         df_raw_train[y_column].unique(), count_of_owners, compute_login)
 
@@ -56,12 +56,12 @@ for count_of_owners, models_to_use, ensemble_based_on_segments, fun, scale, kind
         models_dict, models_to_use, selected_features_dict, y_column, df_raw_train, df_raw_val, df_raw_test, users_to_cv, predict_based_on_whole_pattern, kind_of_patten, ensemble_based_on_segments, ensemble_function, scale_function)
 
     rows.append([scale.split('_')[1], '_'.join(models_to_use),
-                 ensemble_based_on_segments, count_of_owners, fun, kind_of_patten, train_eer, val_eer, test_eer])
+                 ensemble_based_on_segments, count_of_owners, predict_based_on_whole_pattern, fun, kind_of_patten, train_eer, val_eer, test_eer])
     print(len(rows))
 
 
 df_tuning = pd.DataFrame(rows, columns=[
-                         "normalization", "model", "apply_on_segments", "count_of_owners", "function", "kind_of_patten", "train_eer", "val_eer", "test_eer"])
+                         "normalization", "model", "apply_on_segments", "count_of_owners", "predict_based_on_whole_pattern", "function", "kind_of_patten", "train_eer", "val_eer", "test_eer"])
 
 df_tuning.to_csv("../results/cont_ensemble_models.csv",
                  encoding='utf-8', index=False)
